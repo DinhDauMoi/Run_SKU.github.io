baed91dcf50163f6c1f54c744c4864c6a5a32129
unknown <vitinh@nguyenbinh.com>
2023-12-09 21:16:54 +0700

    aaaaa

---
+++ index.js
@@ -0,0 +1,505 @@
+const data = [
+  {
+    question: "Keo deo bap Viet kg",
+    answer: "58628"
+   },
+   {
+    question: "Keo deo tong hop kg",
+    answer: "58629"
+   },
+   {
+    question: "Keo ly socola kg",
+    answer: "58632"
+   },
+   {
+    question: "Keo Socola hoa hong kg",
+    answer: "58633"
+   },
+   {
+    question: "Keo Socola dong tien kg",
+    answer: "58634"
+   },
+   {
+    question: "Keo Socola vuong chum kg",
+    answer: "58635"
+   },
+   {
+    question: "Keo Socola chu nhat kg",
+    answer: "58636"
+   },
+   {
+    question: "Keo sua nhan kem kg",
+    answer: "58637"
+   },
+   {
+    question: "Keo Misoca ngoc trai kg",
+    answer: "58639"
+   },
+   {
+    question: "Thach sua chua 37g",
+    answer: "58640"
+   },
+   {
+    question: "Thach sua chua 16g",
+    answer: "58641"
+   },
+   {
+    question: "Thach rau cau kg",
+    answer: "58642"
+   },
+   {
+    question: "Keo sua dua DUC HANH kg",
+    answer: "56523"
+   },
+   {
+    question: "Keo o mai DUC HANH kg",
+    answer: "56524"
+   },
+   {
+    question: "Thach sua chua nha dam kg",
+    answer: "54113"
+   },
+   {
+    question: "Thach caramen sua kg",
+    answer: "54114"
+   },
+   {
+    question: "Thach tra xanh Matcha Jelly kg",
+    answer: "54115"
+   },
+   {
+    question: "Thach rcau Shahala (3in1) kg",
+    answer: "54116"
+   },
+   {
+    question: "Keo Chobisca kg",
+    answer: "54117"
+   },
+   {
+    question: "Keo Socolate oc que kg",
+    answer: "54118"
+   },
+   {
+    question: "Keo cafe sua Cafemico kg",
+    answer: "54119"
+   },
+   {
+    question: "Keo sua dua Suzumica kg",
+    answer: "54120"
+   },
+   {
+    question: "Thach rau cau ZAIZAI kg",
+    answer: "56903"
+   },
+   {
+    question: "Keo Socola gau truc kg",
+    answer: "56905"
+   },
+   {
+    question: "Keo Socola Pretty kg",
+    answer: "56906"
+   },
+   {
+    question: "Keo deo xoai Mangoca kg",
+    answer: "56908"
+   },
+   {
+    question: "Keo lac sua Lasumi kg",
+    answer: "56907"
+   },
+   {
+    question: "Cu hanh tay HaLan COOP kg",
+    answer: "60645"
+   },
+   {
+    question: "Cu hanh tay tim An Do kg",
+    answer: "55219"
+   },
+   {
+    question: "Cu hanh tay tui luoi kg",
+    answer: "58650"
+   },
+   {
+    question: "Cu nghe kg",
+    answer: "50953"
+   },
+   {
+    question: "Cu rieng",
+    answer: "50887"
+   },
+   {
+    question: "Cu san Kg",
+    answer: "50913"
+   },
+   {
+    question: "Cu san non kg",
+    answer: "58911"
+   },
+   {
+    question: "Cu Su hao Kg",
+    answer: "50775"
+   },
+   {
+    question: "Dau bap COOP kg",
+    answer: "59176"
+   },
+   {
+    question: "Dau Bap Kg",
+    answer: "50186"
+   },
+   {
+    question: "Dau Bo kg",
+    answer: "50796"
+   },
+   {
+    question: "Dau cove Nhat",
+    answer: "50185"
+   },
+   {
+    question: "Dau cove thuong COOP kg",
+    answer: "58956"
+   },
+   {
+    question: "Dau dua Kg",
+    answer: "50042"
+   },
+   {
+    question: "Dau giong Ha Lan",
+    answer: "50187"
+   },
+   {
+    question: "Dau ngot kg",
+    answer: "50112"
+   },
+   {
+    question: "Dau trang kg",
+    answer: "50801"
+   },
+   {
+    question: "Du du bao",
+    answer: "51896"
+   },
+   {
+    question: "Du du xanh",
+    answer: "51925"
+   },
+   {
+    question: "Dua huong",
+    answer: "51592"
+   },
+   {
+    question: "Dua kho nao kg",
+    answer: "51855"
+   },
+   {
+    question: "Dua leo COOP kg",
+    answer: "58670"
+   },
+   {
+    question: "Dua leo dai giong Halan",
+    answer: "52105"
+   },
+   {
+    question: "Dua leo giong Halan co-op",
+    answer: "58675"
+   },
+   {
+    question: "Dua leo Kg",
+    answer: "50914"
+   },
+   {
+    question: "Dua leo Nhat h.co kg-BDAOG",
+    answer: "53907"
+   },
+   {
+    question: "Dua leo thuy canh kg",
+    answer: "50942"
+   },
+   {
+    question: "Gia song kg",
+    answer: "50190"
+   },
+   {
+    question: "Gung kg",
+    answer: "50925"
+   },
+   {
+    question: "Hanh kho hai duong Coop",
+    answer: "60436"
+   },
+   {
+    question: "Hanh kho Hai Duong kg",
+    answer: "59155"
+   },
+   {
+    question: "Hanh la co-op",
+    answer: "58875"
+   },
+   {
+    question: "Hanh la kg",
+    answer: "50207"
+   },
+   {
+    question: "Hanh tim An Do kg",
+    answer: "56904"
+   },
+   {
+    question: "Hanh tim Ly Son kg",
+    answer: "50944"
+   },
+   {
+    question: "Hanh tim P.Rang lot vo",
+    answer: "58898"
+   },
+   {
+    question: "Hanh tim Phan Rang kg",
+    answer: "50740"
+   },
+   {
+    question: "Hanh tim Vinh Chau chum kg",
+    answer: "51673"
+   },
+   {
+    question: "Hanh tim Vinh Chau Kg",
+    answer: "50270"
+   },
+   {
+    question: "Hanh tim Vinh Chau xa kg",
+    answer: "60889"
+   },
+   {
+    question: "He la COOP kg",
+    answer: "53646"
+   },
+   {
+    question: "He la kg",
+    answer: "50206"
+   },
+   {
+    question: "Hung cay kg",
+    answer: "50139"
+   },
+   {
+    question: "Hung lui kg",
+    answer: "50124"
+   },
+   {
+    question: "Keo neo Kg",
+    answer: "50104"
+   },
+   {
+    question: "Khe chua",
+    answer: "51911"
+   },
+   {
+    question: "Kho qua COOP kg",
+    answer: "58669"
+   },
+   {
+    question: "Kho qua tay COOP kg",
+    answer: "54193"
+   },
+   {
+    question: "Kho qua trai kg",
+    answer: "50904"
+   },
+   {
+    question: "Khoai lang kg",
+    answer: "50765"
+   },
+   {
+    question: "Khoai lang mat kg",
+    answer: "51091"
+   },
+   {
+    question: "Khoai lang tim giong Nhat kg",
+    answer: "51304"
+   },
+   {
+    question: "Khoai lang tui luoi kg",
+    answer: "52844"
+   },
+   {
+    question: "Khoai mi kg",
+    answer: "50766"
+   },
+   {
+    question: "Khoai mi lot san kg",
+    answer: "50958"
+   },
+   {
+    question: "Khoai mo got vo kg",
+    answer: "53304"
+   },
+   {
+    question: "Khoai mo Kg",
+    answer: "50915"
+   },
+   {
+    question: "Khoai mon cao kg",
+    answer: "50949"
+   },
+   {
+    question: "Khoai so got vo kg",
+    answer: "50916"
+   },
+   {
+    question: "Khoai so Kg",
+    answer: "50769"
+   },
+   {
+    question: "Khoai tay bi kg",
+    answer: "50805"
+   },
+   {
+    question: "Khoai tay DaLat kg",
+    answer: "50990"
+   },
+   {
+    question: "Khoai tay trang My kg",
+    answer: "53392"
+   },
+   {
+    question: "La cam",
+    answer: "50172"
+   },
+   {
+    question: "La chuoi kg",
+    answer: "50288"
+   },
+   {
+    question: "La dua",
+    answer: "50171"
+   },
+   {
+    question: "La Giang Kg",
+    answer: "50094"
+   },
+   {
+    question: "La lop kg",
+    answer: "50216"
+   },
+   {
+    question: "Mang tay COOP L1 kg",
+    answer: "59326"
+   },
+   {
+    question: "Mang tay COOP L2 kg",
+    answer: "59327"
+   },
+   {
+    question: "Me vat Kg",
+    answer: "50918"
+   },
+   {
+    question: "Muop huong COOP kg",
+    answer: "56500"
+   },
+   {
+    question: "Muop huong Kg",
+    answer: "50919"
+   },
+   {
+    question: "Muop khia Kg",
+    answer: "50755"
+   },
+   {
+    question: "Nam bao ngu trang kg",
+    answer: "50044"
+   },
+   {
+    question: "Nam bao ngu xam kg",
+    answer: "51283"
+   },
+   {
+    question: "Nam moi den kg",
+    answer: "59812"
+   },
+   {
+    question: "Nam rom got vo kg",
+    answer: "50166"
+   },
+   {
+    question: "Nam rom kg",
+    answer: "50802"
+   },
+   {
+    question: "Ngo sen kg",
+    answer: "50214"
+   },
+   {
+    question: "Ngo tay kg",
+    answer: "50211"
+   },
+   {
+    question: "Ngon su su kg",
+    answer: "51082"
+   },
+   {
+    question: "Nha dam kg",
+    answer: "50123"
+   },
+   {
+    question: "Nuoc dua",
+    answer: "51144"
+   },
+   {
+    question: "Ot chuong COOP kg",
+    answer: "58676"
+   },
+   {
+    question: "Ot chuong do DLat kg",
+    answer: "50762"
+   },
+   {
+    question: "Ot chuong vang kg",
+    answer: "50937"
+   },
+   {
+    question: "Ot chuong xanh h.co kg-BDAOG",
+    answer: "53906"
+   },
+];
+const questionInput = document.getElementById("questionInput"); //ô nhập câu hỏi
+const suggestion = document.getElementById("suggestion"); // câu trả lời
+const answer = document.getElementById("answer"); // câu hỏi
+
+questionInput.addEventListener("input", function () {
+  const inputText = questionInput.value; //lấy value input
+  suggestion.innerHTML = "";
+
+  if (inputText.length === 0) {
+    answer.innerHTML = "";
+    return;
+  }
+
+  const matchingQuestions = data.filter(
+    (item) => item.question.toLowerCase().includes(inputText.toLowerCase()) //cho về chữ thường
+  );
+
+  if (matchingQuestions.length > 0) {
+    matchingQuestions.forEach((item) => {
+      const question = document.createElement("div");
+      question.textContent = item.question;
+      suggestion.appendChild(question);
+    });
+    suggestion.style.display = "block";
+  } else {
+    suggestion.style.display = "none";
+  }
+});
+
+suggestion.onclick = (event) => {
+  if (event.target.tagName === "DIV") {
+    const selectedQuestion = event.target.textContent;
+    const selectedAnswer = data.find(
+      (item) => item.question === selectedQuestion
+    ).answer;
+    const formattedAnswer = selectedAnswer.replace(/\$/g, "<br>");
+
+    questionInput.value = selectedQuestion;
+    answer.innerHTML = formattedAnswer;
+    suggestion.style.display = "none";
+  }
+};
+
